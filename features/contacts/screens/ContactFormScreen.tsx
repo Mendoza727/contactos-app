19b4f0246902974cb967b4e6034f4cc60dc6977b
Juan Camilo <juancamilomendozavillegas14@gmail.com>
2026-02-17 19:33:40 -0500

    feat: bootstrap app architecture with modular folder structure and core providers

---
+++ features/contacts/screens/ContactFormScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+import { Text } from "react-native-paper"
+
+
+export const ContactFormScreen = () => {
+    return (
+        <View>
+            <Text>ContactFormScreen</Text>
+        </View>
+    )
+}
---
+++ features/contacts/screens/ContactListScreen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+import { Text } from "react-native-paper"
+
+
+export const ContactListScreen = () => {
+    return (
+        <View>
+            <Text>ContactListScreen</Text>
+        </View>
+    )
+}
